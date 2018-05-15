b6c0b159767a28b3fcd39ced9aa9ff4c4cff6516
Dorian Abad Tovar Díaz <abadtovar@gmail.com>
2018-05-15 15:34:23 -0500

    Added Comments in the code

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,29 @@
+// Elemento en el que irá la presentación
 var wrapper = document.getElementById('DaliShow');
+
+// Dimensiones de la presentación
 var CanvasWidth = wrapper.offsetWidth;
 var CanvasHeight = wrapper.offsetHeight;
 
+// Se crea la presentación en Phaser
 var slideshow = new Phaser.Game( CanvasWidth, CanvasHeight, Phaser.CANVAS, "DaliShow");
 
+// Creando el estado de introducción
 var First =
 {
 	daliAtomicus: "",
 	presentation: "",
 	fadeout: "",
 
+	// Función de precarga, carga las imagenes necesarias al estado
 	preload: function()
 	{
 		slideshow.load.image("DaliAtomicus", "assets/DaliAtomicus.jpg");
 	},
 
+	// Crea la escena del estado cargando la imagen de fondo junto
+	// con las animaciones Fade-in de la presentación y el menú
+	// lateral
 	create: function()
 	{
 		this.presentation = slideshow.add.group();
@@ -28,11 +37,15 @@ var First =
 		$("#intro").fadeTo(1000, 1, function() {});
 	},
 
+	// Retira la foto de presentación para dar paso
+	// al siguente estado entrante
 	shutdown: function() {
 		this.fadeout.start();
 	}
 }
 
+// Creando el estado que controla la presentación e interacción
+// de las 8 pinturas
 var Menu =
 {
 	background: "",
@@ -43,6 +56,7 @@ var Menu =
 	lastX: -1,
 	lastY: -1,
 
+	// Función de precarga, carga las imagenes de las 8 pinturas necesarias al estado
 	preload: function()
 	{
 		slideshow.load.image("paint1", "assets/PersistenciaDeLaMemoria.jpg");
@@ -55,14 +69,21 @@ var Menu =
 		slideshow.load.image("paint8", "assets/SuenoCausadoVueloAbejaAlrededorGranadaSegundoAntesDespertar.jpg");
 	},
 
+	// Crea la escena completa
 	create: function()
 	{
+		// Crea un grupo Phaser y añade la imagen de fondo
+		// colocando una transparencia
 		this.presentation = slideshow.add.group();
 		this.background = slideshow.add.image(0, 0, "paint1");
 		this.background.scale.setTo( CanvasHeight/795 );
 		this.background.alpha = 0.4;
 		this.presentation.add(this.background);
 
+		// Arreglo que contiene las posiciones iniciales x y,
+		// la escala s de la imagen, la escala de presentación c
+		// usada al clickear la imagen y las posiciones sx sy
+		// a las que se desplaza la imagen al clickearla
 		this.positions = [
 			{x: 30,		y: 40, 	s: 0.13, 	c: 0.35, sx: 25, sy: 10 },
 			{x: 300, 	y: 50, 	s: 0.09, 	c: 0.26, sx: 20, sy: 20 },
@@ -74,6 +95,10 @@ var Menu =
 			{x: 250, 	y: 450, s: 0.08, 	c: 0.2, sx: 180, sy: 10 }
 		];
 
+		// Se crean las imagenes y se añaden a un arreglo,
+		// Se escalan y se les permite la opción Drag and drop
+		// y se les coloca un evento para recibir el click
+		// sobre la imagen
 		for( var i = 0; i < this.positions.length; i++ )
 		{
 			this.paintings[i] = slideshow.add.image( this.positions[i].x, this.positions[i].y, "paint"+(i+1));
@@ -86,8 +111,13 @@ var Menu =
 		}
 	},
 
+	// Función que controla el evento cuando el mouse pasa sobre una
+	// imagen y esta se expande un poco para aumentar la visibilidad
 	update: function()
 	{
+		// Si no hay ninguna imagen clickeada, se busca sobre
+		// cual está el mouse y se realiza el efecto de
+		// expandirla
 		if( this.active == -1 ){
 			for( var i = 0; i < this.paintings.length; i++ ){
 				if(this.paintings[i].input.pointerOver()){
@@ -99,6 +129,7 @@ var Menu =
 		}
 	},
 
+	// Función que ayuda a determinar si el evento de ratón es un click o drag and drop
 	isDrag: function()
 	{
 		var distanceFromLastUp = Phaser.Math.distance(slideshow.input.activePointer.positionDown.x, slideshow.input.activePointer.positionDown.y, slideshow.input.activePointer.x, slideshow.input.activePointer.y);
@@ -106,13 +137,20 @@ var Menu =
 		return false;
 	},
 
+	// Función que escucha el click de mouse del usuario
 	listener: function(image, pointer){
+		// Si se determina que es el click izquierdo y
+		// no es un drag and drop
 		if( pointer.button == 0 && !this.isDrag() ){
+			// si no hay foto activa
 			if( this.active == -1 ){
+				// Se guarda la foto activa y su ultima posicion
 				this.active = image.id;
 				this.lastX = image.x;
 				this.lastY = image.y;
 
+				// Se crean las animaciones de las fotos, una para moverla
+				// y la segunda para expandirla a una escala determinada
 				move = slideshow.add.tween(image).to(
 					{ x:  this.positions[this.active].sx, y:  this.positions[this.active].sy },
 					100, Phaser.Easing.Linear.None );
@@ -121,18 +159,26 @@ var Menu =
 					{ x: this.positions[this.active].c, y: this.positions[this.active].c },
 					1000, Phaser.Easing.Linear.None );
 
+				// Se unen las animaciones
 				move.chain(scale);
 				move.start();
 				this.showInfo(this.active);
-			} else {
+			}
+			// Si hay una foto activa
+			else {
+				// Se crean las animaciones, la primera para escalar la foto a pequena
+				// y la segunda para moverla a la posicion previa
 				scale = slideshow.add.tween(image.scale).to(
 					{ x: this.positions[this.active].s, y: this.positions[this.active].s },
 					1000, Phaser.Easing.Linear.None );
 				move = slideshow.add.tween(image).to( { x: this.lastX, y: this.lastY }, 100, Phaser.Easing.Linear.None );
+
+				// Se unen las animaciones
 				move.chain(scale);
 				move.start();
 				this.showInstruct( this.active );
 
+				// Se saca la imagen de activa
 				this.active = -1;
 				this.lastX = -1;
 				this.lastY = -1;
@@ -140,6 +186,8 @@ var Menu =
 		}
 	},
 
+	// Función que cambia el menú lateral a la información
+	// de cada pintura usando JQuery
 	showInfo: function( id )
 	{
 		$("#instruct").fadeTo(1000, 0, function(){
@@ -148,6 +196,8 @@ var Menu =
 		});
 	},
 
+	// Función que retorna el menú lateral de cualquier pintura
+	// al menú de instrucciones usando JQuery
 	showInstruct: function( id )
 	{
 		$("#paint"+(id+1)).fadeTo(1000, 0, function(){
@@ -157,10 +207,17 @@ var Menu =
 	}
 }
 
+// Se crean los dos estados en Phaser
 slideshow.state.add("Presentation", First);
 slideshow.state.add("Menu", Menu);
+
+// Se inicia con el estado de presentación
 slideshow.state.start("Presentation");
 
+// función que inicia la presentación luego de pulsar
+// el botón "Iniciar", cambia el estado en Phaser y
+// cambia el menú lateral por las instrucciones
+// de uso
 function startSlide()
 {
 	slideshow.state.start("Menu", true);
